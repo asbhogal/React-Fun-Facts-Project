57791b61adbe43ac68983c26394588c86c1692a1
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-11-15 16:43:59 +0000

    rendered components to DOM

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import "../scss/index.scss";
+import Header from "./Header";
+import Body from "./Body";
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Body />
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/Body.jsx
@@ -0,0 +1,10 @@
+const Body = () => {
+    return (
+        <div className="body">
+            <p>Test content</p>
+        </div>
+    )
+}
+
+console.log(Body);
+export default Body
