aad6af8444e01e360d95f0e39293c68b43b0aded
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-11-15 19:27:11 +0000

    further adjustments to content and styling

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -5,15 +5,18 @@ const Section = () => {
                 Fun facts about React
             </h2>
             <ul className="FunFactsList">
-                <span className={'BulletPoint'}></span>
-                <li className="FunFactsListItem">Was first released in 2013</li>
+                <li className="FunFactsListItem">                <span className="BulletPoint"></span>Was first released in 2013</li>
+                <span className="BulletPoint"></span>
                 <li className="FunFactsListItem">Was originally created by Jordan Walke</li>
+                <span className="BulletPoint"></span>
                 <li className="FunFactsListItem">Has well over 100K starts on GitHub</li>
+                <span className="BulletPoint"></span>
                 <li className="FunFactsListItem">Is maintained by Facebook</li>
+                <span className="BulletPoint"></span>
                 <li className="FunFactsListItem">Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
         </section>
-    );
+    )
 }
 
 export default Section
