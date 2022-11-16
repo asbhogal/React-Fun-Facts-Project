720b9120afea68343b3d8d6b3ebe564387fbfa6d
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-11-16 13:55:30 +0000

    further content, styling adj. and responsive

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -4,16 +4,19 @@ const Section = () => {
             <h2 className="FunFactsTitle">
                 Fun facts about React
             </h2>
+            <div className="BulletPoints">
+                <span className="BulletPoint BulletPoint-1"></span>
+                <span className="BulletPoint BulletPoint-2"></span>
+                <span className="BulletPoint BulletPoint-3"></span>
+                <span className="BulletPoint BulletPoint-4"></span>
+                <span className="BulletPoint BulletPoint-5"></span>
+            </div>
             <ul className="FunFactsList">
-                <li className="FunFactsListItem">                <span className="BulletPoint"></span>Was first released in 2013</li>
-                <span className="BulletPoint"></span>
-                <li className="FunFactsListItem">Was originally created by Jordan Walke</li>
-                <span className="BulletPoint"></span>
-                <li className="FunFactsListItem">Has well over 100K starts on GitHub</li>
-                <span className="BulletPoint"></span>
-                <li className="FunFactsListItem">Is maintained by Facebook</li>
-                <span className="BulletPoint"></span>
-                <li className="FunFactsListItem">Powers thousands of enterprise apps, including mobile apps</li>
+                <li className="FunFactsListItem FunFactsListItem-1">Was first released in 2013</li>
+                <li className="FunFactsListItem FunFactsListItem-2">Was originally created by Jordan Walke</li>
+                <li className="FunFactsListItem FunFactsListItem-3">Has well over 100K starts on GitHub</li>
+                <li className="FunFactsListItem FunFactsListItem-4">Is maintained by Facebook</li>
+                <li className="FunFactsListItem FunFactsListItem-5">Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
         </section>
     )
