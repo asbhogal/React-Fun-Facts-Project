c7f259b385212c362294bc6e77cb9d0671e390ea
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-11-16 14:11:00 +0000

    adjusted responsive and content positioning

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 const Header = () => {
     return (
         <div className="Header">
+            <span className="ReactFactsTitle">React Facts</span>
             <h1 className="ProjectTitle">React Course - Project 1</h1>
         </div>
     )
